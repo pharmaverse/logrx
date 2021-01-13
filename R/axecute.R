61874842d70442ef519fcf06bfba5fca54848eba
Thomas Bermudez <thomas.a.bermudez@gsk.com>
2021-01-13 17:24:52 +0000

    First draft of the function used to initialise the log object

---
+++ R/axecute.R
@@ -0,0 +1,22 @@
+### Functions to manipulate timber_log
+
+initialise.timber_log <- function() {
+   timber_log <- list(
+      system_info = list(),
+      session_info = list(),
+      namespace = list(),
+      warnings = list(),
+      errors = list(),
+      start_time = lubridate::as_datetime(numeric()),
+      end_time = lubridate::as_datetime(numeric()),
+      run_time = lubridate::duration(),
+      file_path = character(),
+      user = character(),
+      masked_functions = list(),
+      used_packages = list(),
+      log_name = character(),
+      log_path = character()
+   )
+
+   return(timber_log)
+}
