ef3774218d5eb812560138671ef07971dd3f0076
Thomas Bermudez <thomas.a.bermudez@gsk.com>
2021-01-18 14:00:57 +0000

    Renamed the script and the function to initialise the timber_log R object. The R object is initialised as an empty named list.

---
+++ R/log.R
@@ -0,0 +1,15 @@
+### Functions to manipulate timber_log
+
+#' log_config
+#'
+#' Initialises the timber_log object
+#'
+#' @return timber_log object as a named list
+#'
+#' @examples
+#' log_config()
+log_config <- function() {
+   timber_log <- list()
+
+   return(timber_log)
+}
