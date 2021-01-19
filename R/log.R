10a780022ca67f195ee3afa65be18dd6eb8eefa9
Nathan Kosiba <nathan.kosiba@atorusresearch.com>
2021-01-19 19:50:02 +0000

    updated DESCRIPTION to include assertthat as an import and changed license to mit, skeletoned out some basic code for set_log_element

--- R/log.R
+++ R/log.R
@@ -48,8 +48,33 @@ log_config <- function(){
       timber_log[[keys[[key]]]] <- NA
    }
 
-   # Set some tiber_log attributes
+   # Set some timber_log attributes
    # TBD
 
    return(timber_log)
+}
+
+
+#' set_log_element
+#'
+#' Adds values to existing named elements in the timber_log object
+#'
+#' @param el_key the key of the element in timber_log to be updated
+#' @param el_value the value to be added to the timber_log element
+#'
+#' @examples
+#' set_log_element("user", Sys.info()[["user"]])
+set_log_element <- function(el_key, el_value){
+   # check if key is currently in the timber_log_object
+   assert_that(el_key %in% names(timber_log),
+               msg = "el_key provided must already exist in timber_log object")
+
+   # check if element is currently not null
+   if (!is.na(timber_log[el_key])) {
+
+   }
+
+
+   # assign element value to specified element key
+   timber_log[el_key] <- el_value
 }
