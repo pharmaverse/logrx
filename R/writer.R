3893b140e62a440c811f44cb43a54223fa9e0b70
Nathan Kosiba <nathan.kosiba@atorusresearch.com>
2021-01-25 16:44:05 +0000

    consolidate writing functions into same file, rerun documentation

--- R/writer.R
+++ R/writer.R
@@ -1,6 +1,27 @@
-# Writer functions used to format timber.log attributes
+### Functions to format log elements to facilitate the writing of the output log file
 
-#' Format timer.log's metadata attributes
+#' Generic function to format timber.log elements for writing
+#'
+#' @param el_key the key of the element in timber.log to be fetched
+#' @param prefix string to be placed before element value during formatting
+#'
+#' @return formatted element including prefix
+#'
+#' @examples
+#' write_log_element(user, "user running program: ")
+#'
+write_log_element <- function(el_key, prefix) {
+   # get element from log
+   el <- get_log_element(el_key)
+
+   # format element with prefix
+   fmtd_el <- paste0(prefix, el)
+
+   # return formatted element
+   return(fmtd_el)
+}
+
+#' Format timer.log's metadata attributes for writing
 #'
 #' @return A vector of timer.log's metadata attributes
 #' @export
