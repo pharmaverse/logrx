44a30096e09b286e077c6eb3ec807ab9f7c9d2e7
Nathan Kosiba <nathan.kosiba@atorusresearch.com>
2021-06-21 15:27:35 +0000

    refactor functions to get and write the masked functions, update repository link, add unit tests for get functions

--- R/writer.R
+++ R/writer.R
@@ -47,22 +47,18 @@ write_metadata <- function(){
 #' @return A formatted vector of masked functions
 #' @export
 #'
+#' @importFrom purrr imap
+#'
 #' @examples
 #' write_masked_functions()
 #'
 write_masked_functions <- function(){
    masked_functions_list <- get_log_element("masked_functions")
 
-   masked_functions <- c()
-
-   for (i in 1:length(masked_functions_list)){
-      name <- names(masked_functions_list)[[i]]
-      source <- masked_functions_list[[i]]$source
-      masks <- masked_functions_list[[i]]$masks
-      fmask <- paste(masks, collapse = ", ")
-      fmtd <- paste0("function `", name, "` from {", fmask, "} by ", source)
-      masked_functions <- append(masked_functions, fmtd)
-   }
+   masked_functions <- imap(masked_functions_list, ~
+                               paste0("function `", .y,
+                                      "` from {", paste(.x$masks, collapse = ", "),
+                                      "} by ", .x$source))
 
 
    return(masked_functions)
--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -6,15 +6,27 @@ test_that("metadata elements are specified correctly and loaded into a list", {
                   version = sessionInfo()[["otherPkgs"]][["timber"]][["Version"]],
                   license = sessionInfo()[["otherPkgs"]][["timber"]][["License"]],
                   built = sessionInfo()[["otherPkgs"]][["timber"]][["Built"]],
-                  repository_link = NULL
+                  repository_link = "https://github.com/atorus-research/timber"
                ))
 })
 
-test_that("when given a file as an argument a normalized file path to that file will be returned", {
+test_that("when given a file as an argument a normalized file path to that file will be returned by default", {
    expect_equal(get_file_path(file = './test-get.R'), paste0(getwd(),'/test-get.R'))
 })
 
-
-test_that("when given a file as an argument a non-normalized file path to that file will be returned", {
+test_that("when given a file as an argument a non-normalized file path to that file will be returned when specified to not normalize", {
    expect_equal(get_file_path(file = './test-get.R', normalize = FALSE), './test-get.R')
 })
+
+test_that("session info is captured", {
+   expect_identical(get_session_info(), capture.output(sessionInfo()))
+})
+
+test_that("all functions that are masked are found and returned", {
+   expect_identical(names(get_masked_functions()), unique(unlist(conflicts(detail = TRUE))))
+})
+
+test_that("each masked function element contain source and masks elements", {
+   expect_identical(unlist(unname(map(get_masked_functions(), ~ names(.x)))), rep(c("source","masks"), length(get_masked_functions())))
+})
+
