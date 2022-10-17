550853e290da5c66fc16b7077f17736a878f844f
Ben Straub <ben.x.straub@gsk.com>
2022-10-17 01:56:08 +0000

    #97 Simple example for tidylog

---
+++ data-raw/tidylog_ex1.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(tidyr)
+library(tibble)
+suppressMessages(library(tidylog))
+
+data("chickwts")
+
+data_wide <- us_rent_income %>%
+   pivot_wider(
+      names_from = variable,
+      names_glue = "{variable}_{.value}",
+      values_from = c(estimate, moe)) %>%
+   arrange(desc(NAME))
+
+get_states <- distinct(us_rent_income, NAME)
+
+contrived_data <- bind_cols(get_states, 50) %>%
+   rename(number = ...2 )
+
+joined_data <- left_join(data_wide, contrived_data)
