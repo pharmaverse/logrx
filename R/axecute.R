c2b70d82cc0b4b42b83551b0151f56799b56720f
Sam Parmar <107635309+parmsam-pfizer@users.noreply.github.com>
2023-07-12 15:40:48 -0400

    remove remove_log_object arg from axecute

--- R/axecute.R
+++ R/axecute.R
@@ -8,8 +8,6 @@
 #' @param file String. Path to file to execute
 #' @param log_name String. Name of log file
 #' @param log_path String. Path to log file
-#' @param remove_log_object Boolean. Should the log object be removed after
-#' writing the log file? Defaults to TRUE
 #' @param include_rds Boolean. Option to export log object as Rds file.
 #' Defaults to FALSE
 #' @param quit_on_error Boolean. Should the session quit with status 1 on error?
@@ -35,10 +33,12 @@
 #' axecute(file.path(dir, "hello.R"))
 axecute <- function(file, log_name = NA,
                     log_path = NA,
-                    remove_log_object = TRUE,
                     include_rds = FALSE,
                     quit_on_error = TRUE,
                     to_report = c("messages", "output", "result")){
+
+   # remove log object
+   remove_log_object <- TRUE
 
    # lower everything for consistency and check values
    to_report <- map_chr(to_report, tolower)
